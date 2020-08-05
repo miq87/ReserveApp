5f3c571cf81edc9ada7cf2a188dc65f9eeb7bc6b
miq87 <miq3l@wp.pl>
2020-08-05 15:10:40 +0200

    auto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { MembersComponent } from './components/members/members.component';
+import { AuthGuard } from './guard/auth.guard';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'hero', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'hero', component: HeroComponent },
-  { path: 'members', component: MembersComponent }
+  { path: 'hero', component: HeroComponent, canActivate: [AuthGuard] },
+  { path: 'members', component: MembersComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
