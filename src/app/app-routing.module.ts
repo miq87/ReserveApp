7fca5751631851268604717a52033bf3c9481819
MIQ87 <miq3l@wp.pl>
2020-10-01 12:50:22 +0200

    Form Builder added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'booking', component: BookingComponent, pathMatch: 'full' },
   { path: 'hero', component: HeroComponent, canActivate: [AuthGuard] },
-  { path: 'members', component: MembersComponent, canActivate: [AuthGuard] }
+  { path: 'members', component: MembersComponent, canActivate: [AuthGuard] },
   { path: 'fb', component: FormBuilderComponent }
 ];
 
