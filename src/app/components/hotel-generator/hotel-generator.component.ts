c4309e930d70516d1c99011b97feebd441d06cf8
MIQ87 <miq3l@wp.pl>
2020-10-09 15:54:11 +0200

    hotel generator added

--- src/app/components/hotel-generator/hotel-generator.component.ts
+++ src/app/components/hotel-generator/hotel-generator.component.ts
@@ -22,14 +22,14 @@ export class HotelGeneratorComponent implements OnInit {
   }
 
   hotelGenerator() {
+    let startDate = Date.now()
     for(let i = 1; i <= 50; i++) {
       let rName = this.hotelNames[Math.floor((Math.random() * (this.hotelNames.length-1)) + 0)] + ' Hotel'
       let rStreet = this.hotelStreet[Math.floor((Math.random() * (this.hotelStreet.length-1)) + 0)]
       let rCity = this.hotelCity[Math.floor((Math.random() * (this.hotelCity.length-1)) + 0)]
       let rNumber = Math.floor((Math.random() * 100) + 10)
       let rPostalCode = Math.floor((Math.random() * 80000) + 10000)
-      //console.log(`${i}. ${rName} Hotel\n\t${rStreet} ${rNumber}\n\t${rPostalCode} ${rCity}\n`)
-
+      
       let rHotel = new Hotel(null,
         { 'hotelName': rName,
           'street': rStreet + ' ' + rNumber,
@@ -40,5 +40,4 @@ export class HotelGeneratorComponent implements OnInit {
           this.booking_.addNewRandomHotels(rHotel)
     }
   }
-
 }
