8ab2603312f9eb4aac01576820675e7471b83317
MIQ87 <miq3l@wp.pl>
2020-10-09 15:45:51 +0200

    hotel generator added

--- src/app/components/hotel-generator/hotel-generator.component.ts
+++ src/app/components/hotel-generator/hotel-generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hotel } from 'src/app/model/hotel';
 import { BookingService } from 'src/app/services/booking.service';
 
 @Component({
@@ -10,11 +11,10 @@ export class HotelGeneratorComponent implements OnInit {
 
   hotelNames = [ 'Grand', 'Hanza', 'Sheraton', 'Amber', 'Mercure', 'Orbis', 'Radisson Blu',
                  'Rezydent', 'Number One', 'Smart', 'Eva', 'Bryza', 'Szydłowski' ]
-  hotelCity = [ 'Gdańsk', 'Sopot', 'Gdynia', 'Warszawa', 'Kraków', 'Wrocław', 'Poznań', 'Katowice', 'Szczecin' ]
-
   hotelStreet = [ 'Adama Mickiewicza', 'Grunwaldzka', 'Jana Jerzego Haffnera', 'Stefana Czarnieckiego', 'Henryka Sienkiewicza',
-                  'Generała Józefa Fiszera', 'Prezydenta Lecha Kaczyńskiego', 'Stanisława Wyspiańskiego', 'Generała Józefa Hallera',
-                  'Jana Pawła II', 'Kołobrzeska', 'Macieja Płażyńskiego', 'Piastowska', 'Pomorska', 'Kolejowa' ]
+                 'Generała Józefa Fiszera', 'Prezydenta Lecha Kaczyńskiego', 'Stanisława Wyspiańskiego', 'Generała Józefa Hallera',
+                 'Jana Pawła II', 'Kołobrzeska', 'Macieja Płażyńskiego', 'Piastowska', 'Pomorska', 'Kolejowa' ]
+  hotelCity = [ 'Gdańsk', 'Sopot', 'Gdynia', 'Warszawa', 'Kraków', 'Wrocław', 'Poznań', 'Katowice', 'Szczecin' ]
 
   constructor(private booking_: BookingService) { }
 
@@ -23,12 +23,21 @@ export class HotelGeneratorComponent implements OnInit {
 
   hotelGenerator() {
     for(let i = 1; i <= 50; i++) {
-      let rName = this.hotelNames[Math.floor((Math.random() * (this.hotelNames.length-1)) + 0)]
-      let rCity = this.hotelCity[Math.floor((Math.random() * (this.hotelCity.length-1)) + 0)]
+      let rName = this.hotelNames[Math.floor((Math.random() * (this.hotelNames.length-1)) + 0)] + ' Hotel'
       let rStreet = this.hotelStreet[Math.floor((Math.random() * (this.hotelStreet.length-1)) + 0)]
+      let rCity = this.hotelCity[Math.floor((Math.random() * (this.hotelCity.length-1)) + 0)]
       let rNumber = Math.floor((Math.random() * 100) + 10)
-      let rPostalCode = Math.floor((Math.random() * 99999) + 10000)
-      console.log(`${i}. ${rName} Hotel\n\t${rStreet} ${rNumber}\n\t${rPostalCode} ${rCity}\n`)
+      let rPostalCode = Math.floor((Math.random() * 80000) + 10000)
+      //console.log(`${i}. ${rName} Hotel\n\t${rStreet} ${rNumber}\n\t${rPostalCode} ${rCity}\n`)
+
+      let rHotel = new Hotel(null,
+        { 'hotelName': rName,
+          'street': rStreet + ' ' + rNumber,
+          'city': rCity,
+          'state': 'Pomorskie',
+          'postalCode': rPostalCode })
+
+          this.booking_.addNewRandomHotels(rHotel)
     }
   }
 
