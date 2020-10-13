04889631b6f9a600c9110c8145bea1c578dede8d
MIQ87 <miq3l@wp.pl>
2020-10-13 15:12:21 +0200

    hero removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HeroComponent } from './components/hero/hero.component';
 import { MembersComponent } from './components/members/members.component';
 import { AuthGuard } from './guard/auth.guard';
 import { PromiseComponent } from './components/promise/promise.component';
