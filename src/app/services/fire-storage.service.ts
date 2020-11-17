ec29c5083373b29341e7721295ac1909fa886054
MIQ87 <miq3l@wp.pl>
2020-11-17 12:47:39 +0100

    firestorage fixed

--- src/app/services/fire-storage.service.ts
+++ src/app/services/fire-storage.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,20 +9,15 @@ export class FireStorageService {
 
   storage = firebase.storage()
   storageRef = this.storage.ref()
-  numReq = 0
 
   constructor() { }
 
   getMainImage(hotelId: string) {
     var spaceRef = this.storageRef.child(`images/${hotelId}/main_img.jpg`)
-    console.log('main ' + hotelId)
-    console.log(this.numReq++)
     return spaceRef.getDownloadURL()
   }
   getDefaultImage() {
     var spaceRef = this.storageRef.child('images/main_img.jpg')
-    console.log('default')
-    console.log(this.numReq++)
     return spaceRef.getDownloadURL()
   }
   sendMainImage(hotelId: string, file: File) {
