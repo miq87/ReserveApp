2c85ec0cb5f0408430be94ad6b96e7ba87ea2924
MIQ87 <miq3l@wp.pl>
2020-12-23 14:23:28 +0100

    my reservation update

--- src/app/components/my-reservations/my-reservation-item/my-reservation-item.component.ts
+++ src/app/components/my-reservations/my-reservation-item/my-reservation-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Reservation } from 'src/app/models/classes/reservation';
+import { FireStorageService } from 'src/app/services/fire-storage.service';
 
 @Component({
   selector: 'app-my-reservation-item',
@@ -9,7 +11,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 export class MyReservationItemComponent implements OnInit {
 
-  @Input() myReservation
+  @Input() myReservation: Reservation
+  hotelMainImg: string
 
   myResForm = this.fb.group({
     roomId: ['', Validators.required],
@@ -19,10 +22,25 @@ export class MyReservationItemComponent implements OnInit {
     notice: [''],
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private _fs: FireStorageService) { }
 
   ngOnInit(): void {
-    console.log(this.myReservation)
+
+    this.myResForm.patchValue(this.myReservation)
+    console.log(this.myResForm.value)
+
+    this._fs.getMainImage(this.myReservation.hotelId).then(data => {
+      console.log(data)
+      this.hotelMainImg = data
+    })
+    .catch(err => {
+      console.log(err.code)
+      this._fs.getDefaultImage().then(data => {
+        console.log(data)
+        this.hotelMainImg = data
+      })
+    })
+
   }
 
 }
