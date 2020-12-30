96f2af67f420b6329503b6f1a8e12a5ffe2fa899
MIQ87 <miq3l@wp.pl>
2020-12-30 14:57:53 +0100

    date

--- src/app/components/my-reservations/my-reservation-item/my-reservation-item.component.ts
+++ src/app/components/my-reservations/my-reservation-item/my-reservation-item.component.ts
@@ -1,6 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { pipe } from 'rxjs';
+import { Hotel } from 'src/app/models/classes/hotel';
 import { Reservation } from 'src/app/models/classes/reservation';
+import { BookingService } from 'src/app/services/booking.service';
 import { FireStorageService } from 'src/app/services/fire-storage.service';
 import { ReservationsService } from 'src/app/services/reservations.service';
 
@@ -12,8 +16,11 @@ import { ReservationsService } from 'src/app/services/reservations.service';
 
 export class MyReservationItemComponent implements OnInit {
 
-  @Input() myReservation: Reservation
+  @Input() myRes: Reservation
+  hotel: Hotel
   hotelMainImg: string
+  pipe = new DatePipe('en-US')
+  dateSt: string
 
   myResForm = this.fb.group({
     resId: ['', Validators.required],
@@ -25,15 +32,22 @@ export class MyReservationItemComponent implements OnInit {
     notice: [''],
   })
 
-  constructor(private fb: FormBuilder, private _fs: FireStorageService, private _res: ReservationsService) { }
+  constructor(
+    private fb: FormBuilder,
+    private _fs: FireStorageService,
+    private _res: ReservationsService,
+    private _bs: BookingService) { }
 
   ngOnInit(): void {
 
-    this.myResForm.patchValue(this.myReservation)
+    this.myResForm.patchValue(this.myRes)
 
-    console.log(this.myResForm.value)
+    this._bs.getHotelDetails(this.myRes.hotelId).then((data: Hotel) => {
+      this.hotel = data
+      console.log(this.hotel)
+    })
 
-    this._fs.getMainImage(this.myReservation.hotelId).then(data => {
+    this._fs.getMainImage(this.myRes.hotelId).then(data => {
       this.hotelMainImg = data
     })
     .catch(err => {
@@ -46,7 +60,7 @@ export class MyReservationItemComponent implements OnInit {
   }
 
   onDeleteReservation() {
-    this._res.deleteReservation(this.myReservation.resId)
+    this._res.deleteReservation(this.myRes.resId)
   }
 
 }
