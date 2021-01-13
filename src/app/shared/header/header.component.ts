2ffc8bc98b9876512b72a3d50cdd01b383661584
MIQ87 <miq3l@wp.pl>
2021-01-13 13:34:11 +0100

    register/login fixed

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -15,7 +15,6 @@ export class HeaderComponent implements OnInit {
   constructor(private _auth: AuthService) { }
 
   ngOnInit() {
-    this.handleBurger()
     this._auth.getCurrentUser(user => {
       if (user) {
         this.currentUser = user
@@ -27,21 +26,6 @@ export class HeaderComponent implements OnInit {
       else { this.isLogged = false }
     })
   }
-
-  handleBurger() {
-    const burger = document.querySelector('.burger')
-    const nav = document.querySelector('.nav-links')
-    const navLinks = document.querySelectorAll('.nav-links li')
-
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active')
-      navLinks.forEach((link) => {
-        link.classList.add('nav-links-fade')
-      })
-      burger.classList.toggle('toggle')
-    })
-  }
-
   onLogout() {
     this._auth.logout()
   }
