adc70fccce14c1b23b7db4f4bda65e3e310b4206
MIQ87 <miq3l@wp.pl>
2021-02-14 19:13:48 +0100

    fixed

--- src/app/services/fire-storage.service.ts
+++ src/app/services/fire-storage.service.ts
@@ -18,16 +18,8 @@ export class FireStorageService {
     return spaceRef.getDownloadURL()
   }
 
-  // async sendMainImage(hotelId: string, file: File) {
-  //   await this.storageRef.child(`images/${hotelId}/main_img.jpg`).put(file).then(() => {
-  //     console.log(`Dodałem 'main_img.jpg' do hotelu o ID '${hotelId}'!`)
-  //   }).catch(err => {
-  //     console.log(err.message)
-  //   })
-  // }
-
   sendImage(hotelId: string, file: File, main?: boolean) {
-    if(main) {
+    if (main) {
       var uploadTask = this.storageRef.child(`images/${hotelId}/main_img.jpg`).put(file)
     }
     else {
@@ -43,39 +35,6 @@ export class FireStorageService {
       });
     })
   }
-
-  // getMainImage(hotelId: string): Promise<string> {
-  //   var spaceRef = this.storageRef.child(`images/${hotelId}/main_img.jpg`)
-  //   return new Promise((resolve, reject) => {
-  //     spaceRef.getDownloadURL().then(url => {
-  //       resolve(url)
-  //     })
-  //       .catch(err => {
-  //         console.log(err.code)
-  //         this.getDefaultImage().then(url => {
-  //           resolve(url)
-  //         })
-  //           .catch(err => {
-  //             reject(err)
-  //           })
-  //       })
-  //   })
-  // }
-
-  // async getAllImages(hotelId: string): Promise<string[]> {
-  //   var listRef = this.storageRef.child(`images/${hotelId}`)
-  //   var urlList = []
-  //   await listRef.listAll().then(res => {
-  //     res.items.forEach(itemRef => {
-  //       itemRef.getDownloadURL().then(url => {
-  //         urlList.push(url)
-  //       })
-  //     })
-  //   }).catch(err => {
-  //     console.log(err)
-  //   })
-  //   return urlList
-  // }
 
   async deleteImg(imgUrl: string) {
     await this.storage.refFromURL(imgUrl).delete().then(() => {
